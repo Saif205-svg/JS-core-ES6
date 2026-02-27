a760597ec33e029c8db399410318b93bdd0bf8e5
Saif205-svg <rafiuzzamansaif005@gmail.com>
2026-02-27 19:50:42 +0600

    null vs undefined

--- undefined.js
+++ undefined.js
@@ -1,2 +1,17 @@
 let data;
 console.log(typeof data);
+
+const sum = (a,b) =>{
+    console.log(a,b);
+}
+sum(1); //return 1 undefined
+
+const student= {
+    name: 'rafi',
+    age: 20,
+}
+ console.log(student.marks); //return undefined
+ console.log(student.name) //return 'rafi'
+
+let a = null;
+console.log(typeof null);
